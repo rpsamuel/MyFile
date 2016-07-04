92e0b3906b928668972e74b881e8c4ad1408baaa
rpsamuel <roderic.samuel@ebitscorp.com>
2016-07-04 12:25:24 -0400

    Update MyFirstPy.py

--- MyFirstPy.py
+++ MyFirstPy.py
@@ -1,1 +1,8 @@
 #---My First Python written on GitHUB
+#!/usr/bin/env python
+logfile = open("/var/log/syslog", "r")
+for line in logfile:
+    line_split = line.split()
+    print line_split
+    list = line_split[0], line_split[1], line_split[2], line_split[4]
+    print list
